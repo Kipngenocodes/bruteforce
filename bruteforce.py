43d9ef3e8519c30b11f4c4a8b8f983acb4b0c50e
Kipngenocodes <kipngenoalex.c@gmail.com>
2024-08-26 11:00:49 -0700

    first message

---
+++ bruteforce.py
@@ -0,0 +1,5 @@
+#  The purpose of the program is to check on password 
+# it return outcome whether it is correct or not.
+#  The program will check the password with the following rules:
+# 1. The password should be at least 8 characters long.
+# 2. The password should contain at least one uppercase letter.
